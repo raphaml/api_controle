02f263138e46df974e1eccda69d709e8e98bf3d6
Raphael Melo de Lima <raphaelmelolima@gmail.com>
2021-04-15 11:50:07 -0300

    estrutura inicial e primeira migration

--- source/app/db/__init__.py
+++ source/app/db/__init__.py
@@ -1,2 +1,3 @@
 from db.main import Base
 from db.main import engine
+from db.main import SessionLocal
--- source/app/main.py
+++ source/app/main.py
@@ -1,18 +1,34 @@
 import uvicorn
 from fastapi import FastAPI
 
-
 import models
 from db import engine
+from db import SessionLocal
 from core.config import settings
+from schemas import User
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/users/", response_model=User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = db.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
 
 if __name__ == '__main__':
     uvicorn.run(
